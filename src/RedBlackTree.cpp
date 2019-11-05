436b5da942fc6b8f5b4550000f304cc72870fc1a
briang0 <brianguidarini@gmail.com>
2019-11-05 16:34:12 -0600

    Initial commit

---
+++ src/RedBlackTree.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+using namespace std
+
+struct node {
+  int key;
+  bool nil;
+  struct node* left;
+  struct node* right;
+  bool color;
+
+  construct(node* parent, int keyIn){
+    if (parent == NULL){
+      nil = true;
+      left = nullptr;
+      right = nullptr;
+      color = false;
+    }else {
+
+    }
+    key = key;
+  }
+//  mutex m;
+}
+
+class RedBlackTree {
+public:
+  node* root
+  node* nil =
+
+  void insert() {
+
+  }
+
+  void remove() {
+
+  }
+
+  void balance() {
+
+  }
+
+}
---
+++ src/node.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+using namespace std;
+
+class node{
+public:
+  node* left;
+  node* right;
+  node* parent;
+  int color;
+}
